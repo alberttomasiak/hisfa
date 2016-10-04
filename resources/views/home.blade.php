1c7e75a2fc3ba7f5c7571ec90eb520db2af912ad
Tom V <tom.vanderheyden01@gmail.com>
2016-10-04 23:27:40 +0200

    Dashboard skeleton added, sample blade files made Added plugins to composer => composer install

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,99 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+    <div class="container">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-6">
+                    <div class="panel panel-default">
+                        <div class="panel-heading"><span class="fx-bold">PRIME</span> Silos</div>
 
-                <div class="panel-body">
-                    You are logged in!
+                        <div class="panel-body">
+                            <div class="row">
+                                <div class="col-sm-3">
+                                    <img src="img/gauge-icon.png" alt="Img" style="height: 25px;">
+                                </div>
+                                <div class="col-sm-3">
+                                    <img src="img/gauge-icon.png" alt="Img" style="height: 25px;">
+                                </div>
+                                <div class="col-sm-3">
+                                    <img src="img/gauge-icon.png" alt="Img" style="height: 25px;">
+                                </div>
+                                <div class="col-sm-3">
+                                    <img src="img/gauge-icon.png" alt="Img" style="height: 25px;">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">WASTE Silos</div>
+
+                        <div class="panel-body">
+                            <div class="row">
+                                <div class="col-sm-3">
+                                    <img src="img/gauge-icon.png" alt="Img" style="height: 25px;">
+                                </div>
+                                <div class="col-sm-3">
+                                    <img src="img/gauge-icon.png" alt="Img" style="height: 25px;">
+                                </div>
+                                <div class="col-sm-3">
+                                    <img src="img/gauge-icon.png" alt="Img" style="height: 25px;">
+                                </div>
+                                <div class="col-sm-3">
+                                    <img src="img/gauge-icon.png" alt="Img" style="height: 25px;">
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+                <div class="col-sm-6">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">Stock</div>
+
+                        <div class="panel-body">
+                            <div class="row">
+                                <div class="col-sm-3">
+                                    <img src="../../public/img/gauge-icon.png" alt="Img">
+                                </div>
+                                <div class="col-sm-3"></div>
+                                <div class="col-sm-3"></div>
+                                <div class="col-sm-3"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">Advanced data</div>
+
+                        <div class="panel-body">
+                            <div class="row">
+                                <div class="col-sm-3">
+                                    <img src="../../public/img/gauge-icon.png" alt="Img">
+                                </div>
+                                <div class="col-sm-3"></div>
+                                <div class="col-sm-3"></div>
+                                <div class="col-sm-3"></div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-</div>
+    {{--<div class="row">--}}
+    {{--<div class="col-md-8 col-md-offset-2">--}}
+    {{--<div class="panel panel-default">--}}
+    {{--<div class="panel-heading">Dashboard</div>--}}
+
+    {{--<div class="panel-body">--}}
+    {{--</div>--}}
+    {{--</div>--}}
+    {{--</div>--}}
+    {{--</div>--}}
+    {{--</div>--}}
+    {{--</div>--}}
+    {{--</div>--}}
 @endsection
