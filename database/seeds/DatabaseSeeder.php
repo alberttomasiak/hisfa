b641a29d81b2b1d8c682c7dedb96f459f48c21f9
Tom V <tom.vanderheyden01@gmail.com>
2016-10-05 13:43:38 +0200

    2/2 Seeder classes aan database seeder toegevoegd

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
         // $this->call(UsersTableSeeder::class);
 		$this->call(SilosTableSeeder::class);
 		$this->call(StocksTableSeeder::class);
+        $this->call(BlocksTableSeeder::class);
     }
 }
