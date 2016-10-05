11f0d35bbebcfc642ceadf1554998a53f9dedf72
Joren De Maeyer <demaeyer.joren@gmail.com>
2016-10-05 17:49:33 +0200

    changed font to 'Lato'

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
 
     <title>Laravel</title>
     <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900" rel="stylesheet">
     <link href="/css/app.css" rel="stylesheet">
 
 </head>
