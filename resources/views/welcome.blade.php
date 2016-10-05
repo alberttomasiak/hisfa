4e5c930ce2a03767177b371adbd82062ad5e5517
Fabio Van Dooren <fabio.vandooren@hotmail.com>
2016-10-05 15:53:24 +0200

    triangles deleted

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -32,25 +32,6 @@
         <svg width="240px" height="100%" id="col5">
             <rect width="120px" height="120px" x="60px" y="60px" class="bubble" id="bub5" />
         </svg>
-        <!--Here is a triangle-->
-        <svg width="200px" height="100%" id="col6">
-            <polygon points="50,150 100,50 150,150" class="bubble" id="bub6" />
-        </svg>
-        <svg width="200px" height="100%" id="col7">
-            <rect width="100px" height="100px" x="50px" y="50px" class="bubble" id="bub7" />
-        </svg>
-        <svg width="200px" height="100%" id="col8">
-            <rect width="100px" height="100px" x="50px" y="50px" class="bubble" id="bub8" />
-        </svg>
-        <svg width="200px" height="100%" id="col9">
-            <rect width="100px" height="100px" x="50px" y="50px" class="bubble" id="bub9" />
-        </svg>
-        <svg width="200px" height="100%" id="col10">
-            <rect width="100px" height="100px" x="50px" y="50px" class="bubble" id="bub10" />
-        </svg>
-        <svg width="100px" height="100%" id="col11">
-            <rect width="50px" height="50px" x="25px" y="25px" class="bubble" id="bub11" />
-        </svg>
     </div>
     
 </body>
