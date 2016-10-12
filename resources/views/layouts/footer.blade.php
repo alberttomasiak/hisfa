18169d8cdce7824ad9e1ecb109032d2b5d8bf490
Joren De Maeyer <demaeyer.joren@gmail.com>
2016-10-12 15:08:22 +0200

    sticky footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,2 +1,22 @@
-<div class="footer">
+<div class="footer footer-nav-icons">
+    <div class="fp-footer">
+        <div class="fp-content active">
+            <a>
+                <i class="icon-dashboard badge-on-icon"></i>
+                <h6>DASHBOARD</h6>
+            </a>
+        </div>
+        <div class="fp-content">
+            <a>
+                <i class="icon-stock badge-on-icon"></i>
+                <h6>STOCK</h6>
+            </a>
+        </div>
+        <div class="fp-content">
+            <a>
+                <i class="icon-rapports badge-on-icon"></i>
+                <h6>RAPPORTEN</h6>
+            </a>
+        </div>
+    </div>
 </div>
