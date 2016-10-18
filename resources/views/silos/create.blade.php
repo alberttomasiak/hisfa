208916f3c5be2da2d31359ddaee1d663a4e7002f
Yame- <yannick@yame.be>
2016-10-18 08:57:15 +0200

    Silo’s nav weggehaald, en mogelijkheid om volume up te daten

--- resources/views/silos/create.blade.php
+++ resources/views/silos/create.blade.php
@@ -1,20 +1,38 @@
 @extends('layouts.app')
 
-@include('silos.nav')
+<!--@include('silos.nav')-->
 
 @section('content')
 
+@if( isset($method) && $method == 'edit' )
+<form method="post" action="{{ action('SilosController@update', $silo->id) }}"> 
+@else
 {!! Form::open(['action' => 'SilosController@store']) !!}
+@endif;
+
+	{{ csrf_field() }}
+
+	{!! Form::label('Wat zit er in de Silo?') !!}
 
 	{!! Form::text('content'); !!}
 
+	{!! Form::label('Numer van de silo'); !!}
+
 	{!! Form::number('number', ''); !!}
+
+	{!! Form::label('Type'); !!}
 
 	{!! Form::select('type', ['stock' => 'Stock', 'waste' => 'Waste'], $type) !!}
 
-	{!! Form::hidden('volume', '0' ); !!}
+	{!! Form::label('Volume in de Silo'); !!}
 
+	{!! Form::number('volume', $silo->volume); !!}
+
+	@if( $button )
+	{!! Form::submit($button); !!}
+	@else
 	{!! Form::submit('Silo toevoegen'); !!}
+	@endif
 
 {!! Form::close() !!}
 
