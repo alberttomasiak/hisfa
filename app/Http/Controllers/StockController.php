e9367af3f998d5a6841ce8cd0632017125c726e3
Yame- <yannick@yame.be>
2016-10-18 15:43:02 +0200

    Stock controller update

--- app/Http/Controllers/StockController.php
+++ app/Http/Controllers/StockController.php
@@ -15,7 +15,8 @@ class StockController extends Controller
      */
     public function index()
     {
-        //
+        return view('stock.index')
+               ->with('title', 'Stock');
     }
 
     /**
