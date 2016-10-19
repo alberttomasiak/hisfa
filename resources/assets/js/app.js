bd5bb54ea860604e6e23910602361e11387d0409
Joren De Maeyer <demaeyer.joren@gmail.com>
2016-10-19 15:59:11 +0200

    Edit silo layout

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -18,3 +18,46 @@ Vue.component('example', require('./components/Example.vue'));
 const app = new Vue({
     el: 'body'
 });
+
+$('.edit-silo__block-form #name').keyup(function(e) {
+    // Do something
+
+});
+
+$('.edit-silo__block-form #number').keyup(function(e) {
+    // Do something
+    console.log('Number has changed');
+});
+
+$('.edit-silo__block-form #type').keyup(function(e) {
+    // Keyup does not work
+
+});
+
+$('.edit-silo__block-form #volume').keyup(function(e) {
+    // Do something
+    var amount = parseInt(e.target.value);
+
+    var silo__top = $('.silo__image__top');
+    var silo__middle = $('.silo__image__middle');
+    var silo__bottom = $('.silo__image__bottom');
+
+    silo__top.removeClass();
+    silo__middle.removeClass();
+    silo__bottom.removeClass();
+
+    $('.silo__percentage').html(amount + '%');
+    if(amount >= 90) {
+        silo__top.addClass('silo__image__top').addClass('full');
+        silo__middle.addClass('silo__image__middle').addClass('full');
+        silo__bottom.addClass('silo__image__bottom').addClass('full');
+    } else if(amount >= 50) {
+        silo__top.addClass('silo__image__top').addClass('default');
+        silo__middle.addClass('silo__image__middle').addClass('medium');
+        silo__bottom.addClass('silo__image__bottom').addClass('medium');
+    } else {
+        silo__top.addClass('silo__image__top').addClass('default');
+        silo__middle.addClass('silo__image__middle').addClass('default');
+        silo__bottom.addClass('silo__image__bottom').addClass('empty');
+    }
+});
