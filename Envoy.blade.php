8bdb17dd495d25f2725b1aba18313f8056f968d0
Albert Tomasiak <tomasiakalbert@gmail.com>
2016-11-16 13:38:00 +0100

    envoy.blade.php

---
+++ Envoy.blade.php
@@ -0,0 +1,15 @@
+@servers(['hisfa' => ['root@139.162.181.121']])
+
+@task('deploy_hisfa', ['on' => 'hisfa'])
+    cd /var/www/html/hisfa5
+    php artisan down
+    git reset --hard HEAD
+    git pull origin develop
+    php composer.phar dump-autoload
+    php artisan migrate --force
+    php artisan up
+    composer update
+    npm install
+    gulp
+    gulp watch
+@endtask
